ea0007f08991b5a80290f386626fdbba46161087
Lajos Seyler <lajos.seyler@gmail.com>
2024-06-01 12:53:15 +0200

    Add send_verify_signup_email helper function

---
+++ users/helpers.py
@@ -0,0 +1,33 @@
+import mailtrap as mt
+from django.conf import settings
+
+
+def send_verify_signup_email(receipient_address, user_id, otp):
+    url = f"{settings.DOMAIN}/api/v1/verify-email/?uid={user_id}&otp={otp}"
+
+    mail = mt.Mail(
+        sender=mt.Address(
+            email="django-tutorial@demomailtrap.com", name="Mailtrap Test"
+        ),
+        to=[mt.Address(email=receipient_address)],
+        subject="Active your account!",
+        html=f"""
+            Hello,<br>
+            <br>
+            thank you for signing up to django-tutorial!<br>
+            <br>
+            Please activate your account by clicking the following link:<br>
+            <br>
+            <a href="{url}" target="_blank">Activate account</a><br>
+            <br>
+            If the link doesn't work, please visit the following URL:<br>
+            <br>
+            {url}<br>
+            <br>
+            Best regards,<br>
+            Django tutorial team
+        """,
+    )
+
+    client = mt.MailtrapClient(token=settings.MAILTRAP_TOKEN)
+    client.send(mail)
